a08a7049579e242303591b8e956ad6cfd4ed7f2f
Rômulo Henri <romulohenri000@gmail.com>
2023-09-17 19:21:35 -0300

    update: Contacts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var btnPricing = document.getElementById('pricing')
 
-btnPricing.addEventListener('click', () => {
-	alert('Calculadora de Orçamento Indisponível! Está em desenvolvimento.')
-})
+// btnPricing.addEventListener('click', () => {
+// 	alert('Calculadora de Orçamento Indisponível! Está em desenvolvimento.')
+// })
