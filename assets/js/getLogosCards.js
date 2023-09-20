4f654db41f7659916539a8e08c7a7e2213d74827
Rômulo Henri <romulohenri000@gmail.com>
2023-09-19 22:07:53 -0300

    add: Logos section

---
+++ assets/js/getLogosCards.js
@@ -0,0 +1,54 @@
+var cardsContainer = document.querySelector(".logos-container")
+var trashHTML = cardsContainer.innerHTML
+
+cardsContainer.innerHTML = ``
+const pathData = "../../logos.json";
+const urlData = "https://romhenri.github.io/rdesign/logos.json";
+
+getData()
+
+function getData() {
+  fetch(urlData)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        data.forEach(item => {
+          createCard(item)
+        });
+        initGight()
+      })
+      .catch(error => console.error('Erro:', error));
+}
+
+function createCard(item) {
+  var card = document.createElement("div")
+  card.className = "col-xl-3 col-lg-4 col-md-6"
+  card.innerHTML = `
+    <div class="gallery-item h-100">
+      <img
+        src="${item.src}"
+        class="img-fluid"
+        alt="${item.name}"
+      />
+      <div
+        class="gallery-links d-flex align-items-center justify-content-center"
+      >
+        <a
+          href="${item.src}"
+          title="${item.name}"
+          class="glightbox preview-link"
+          ><i
+            class="bi bi-arrows-angle-expand"
+          ></i
+        ></a>
+      </div>
+    </div>
+  `
+  cardsContainer.appendChild(card)
+}
+
+function initGight() {
+  const glightbox = GLightbox({
+    selector: '.glightbox',
+  })
+}
